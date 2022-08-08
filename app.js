128799ba5dc31fb3b0fde22094a7361d59d7702b
jonijaas <joni.jaaskelainen2@metropolia.fi>
2022-08-08 12:43:48 +0300

    Added first workflow pipeline

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('frontend/build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
